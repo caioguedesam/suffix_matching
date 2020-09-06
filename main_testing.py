d9cc7882909abefe0e60c9caf144faf409c72831
Caio Guedes <caioguedesam@gmail.com>
2020-09-06 13:50:58 -0300

    Finished main and testing script. Needs test cases

--- main_testing.py
+++ main_testing.py
@@ -1,5 +1,16 @@
 import sys
 import tree
+import time
+from guppy import hpy
+
+h = hpy()
+startTime = time.time()
+
+def PrintTimeAndMemoryAtInstant(message = ''):
+    if message != '':
+        print('{0}:'.format(message))
+    print('+++ Tempo total: {:10.3f} segundos'.format(time.time() - startTime))
+    print('+++ Memória utilizada: {:10.3f} megabytes'.format(h.heap().size / 1000000))
 
 def ParseGenome(genome) -> str:
     split = genome.split('\n', 1)
@@ -12,12 +23,15 @@ def ParseGenome(genome) -> str:
 def GetLongestRepeatedSubstring(text):
     # Criando árvore de sufixos.
     t = tree.Tree(text)
+    PrintTimeAndMemoryAtInstant('Após construção da árvore de sufixos do texto')
 
     # Pegando resultados da busca na árvore.
     node = t.Search()
     result = node.GetAccumulatedSubstring(text) + [node.GetChildCount()]
+    PrintTimeAndMemoryAtInstant('Após encontrar a maior substring repetida')
     return result
 
+PrintTimeAndMemoryAtInstant('Antes de ler e armazenar o arquivo')
 filePath = 'sarscov2.fasta'
 if(len(sys.argv) > 1):
     filePath = sys.argv[1]
